6d0409f14c5cc6d85c6fc3dcf5adfc27d2951f49
wuyunqiang <wuyunqiang@kuaishou.com>
2023-06-14 21:18:32 +0800

    feat: add dynamic-task

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from "rollup";
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import Json from "@rollup/plugin-json";
+import babel from "@rollup/plugin-babel";
+import { readFileSync } from "fs";
+const PKG_JSON = JSON.parse(readFileSync('package.json', {encoding: 'utf8'}));
+
+const external = [
+  ...Object.keys(PKG_JSON.devDependencies || {}),
+  ...Object.keys(PKG_JSON.peerDependencies || {}),
+];
+export default defineConfig({
+  input: {
+    index: "src/index.ts",
+  },
+  output: [
+    {
+      format: "es",
+      dir: "dist",
+      exports: "auto",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    Json(),
+    resolve({
+      browser: true,
+      extensions: [".js", ".ts"],
+      preferBuiltins: true,
+    }),
+    typescript(),
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      compact: true,
+      comments: false,
+      extensions: [".ts"],
+      include: ["src/**/*.ts"],
+    }),
+  ],
+  external,
+});
