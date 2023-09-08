f44751b6b6b60df460b1b0a678b6b6d4285fc6c9
wuyunqiang <wuyunqiang@kuaishou.com>
2023-09-08 16:16:38 +0800

    feat: 优化yieldToMain实现

--- src/yield-to-main.ts
+++ src/yield-to-main.ts
@@ -1,9 +1,27 @@
 /**
  * 让出主线程 
  * 用户可以手动分解长任务
+ * 优先使用MessageChannel宏任务&无延迟 阻断执行让出主线程
+ * 使用setTimeOut兼容
  * @returns Promise
  */
 export const yieldToMain = async () => {
+  if(typeof MessageChannel !== 'undefined') {
+    let _resolve:(value?: any)=>void;
+    const p = new Promise(resolve => {
+      _resolve = resolve;
+    })
+    const channel = new MessageChannel(); // 建立宏任务的消息通道
+    const port1 = channel.port1;
+    const port2 = channel.port2;
+
+    port2.onmessage = () => {
+      _resolve();
+    }
+    port1.postMessage('yield')
+    return p;
+  }
+  
   return new Promise(resolve => {
     setTimeout(resolve, 0);
   });
