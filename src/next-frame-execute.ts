84212fa339910414a6ab78e71c98614173b6bf98
wuyunqiang <wuyunqiang@kuaishou.com>
2023-09-12 12:03:44 +0800

    feat: 优化nextFrameExecute实现

--- src/next-frame-execute.ts
+++ src/next-frame-execute.ts
@@ -8,22 +8,40 @@ import { Resolve, Task } from "../type/task";
  * @returns {Promise}
  */
 export const nextFrameExecute = async (task: Task) => {
-    let _resolve: Resolve;
-    let _reject: Resolve;
-    const p = new Promise((resolve, reject) => {
-      _resolve = resolve;
-      _reject = reject;
-    });
-    if (typeof requestAnimationFrame !== "undefined") {
-      requestAnimationFrame(() => {
-        requestAnimationFrame(()=>{
-          Promise.resolve(task()).then(_resolve, _reject);
-        })
-      });
-      return p;
-    }
-    setTimeout(() => {
+  let _resolve: Resolve;
+  let _reject: Resolve;
+  const p = new Promise((resolve, reject) => {
+    _resolve = resolve;
+    _reject = reject;
+  });
+
+  if (typeof MessageChannel !== 'undefined') {
+    const channel = new MessageChannel(); // 建立宏任务的消息通道
+    const port1 = channel.port1;
+    const port2 = channel.port2;
+
+    port2.onmessage = () => {
       Promise.resolve(task()).then(_resolve, _reject);
-    }, 16);
+    }
+    port1.postMessage('next')
     return p;
-  };
+  }
+
+  /**下一帧执行执行 
+   * Any rAFs queued in your event handlers will be executed in the same frame.
+   * Any rAFs queued in a rAF will be executed in the next frame
+   * **/
+  if (typeof requestAnimationFrame !== "undefined") {
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        Promise.resolve(task()).then(_resolve, _reject);
+      })
+    });
+    return p;
+  }
+
+  setTimeout(() => {
+    Promise.resolve(task()).then(_resolve, _reject);
+  }, 16);
+  return p;
+};
