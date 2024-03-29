cc5c15373964e125571b9633395fca90ff9dc89c
wuyunqiang <wuyunqiang@kuaishou.com>
2024-03-29 12:18:20 +0800

    feat: task-cancel-able

--- src/task-cancelable.ts
+++ src/task-cancelable.ts
@@ -1,4 +1,4 @@
-import { PromiseCancel, Resolve, Task } from "../type/task";
+import { PromiseCancel, Resolve } from "../type/task";
 
 /**
  * 将一个promise转换为一个可取消的promise
@@ -6,30 +6,30 @@ import { PromiseCancel, Resolve, Task } from "../type/task";
  * @returns {Promise} 返回具有cancel()&isCancel()的promise对象
  */
 export const TaskCancelable = (task: Promise<any>) => {
-    let _reject: Resolve;
-    let isCancel = false;
-    const _status = Symbol("cancel");
-    const cancelP = new Promise((resolve, reject) => {
-      _reject = reject;
-    });
-    const p = Promise.race([task, cancelP]) as PromiseCancel;
-    /***
-     * 调用cancel时可能promise状态已经变为成功,
-     * 所以不能在cancel里面改变isCancel
-     * 只有catch的原因是cancel才代表被取消成功了
-     */
-    p.catch((reason) => {
-      if (reason === _status) {
-        isCancel = true;
-      }
-    });
-  
-    p.cancel = () => {
-      _reject(_status);
-    };
-    p.isCancel = () => {
-      return isCancel;
-    };
+  let _reject: Resolve;
+  let isCancel = false;
+  const _status = Symbol("cancel");
+  const cancelP = new Promise((resolve, reject) => {
+    _reject = reject;
+  });
+  const p = Promise.race([task, cancelP]) as PromiseCancel;
+  /***
+   * 调用cancel时可能promise状态已经变为成功,
+   * 所以不能在cancel里面改变isCancel
+   * 只有catch的原因是cancel才代表被取消成功了
+   */
+  p.catch((reason) => {
+    if (reason === _status) {
+      isCancel = true;
+    }
+  });
+
+  p.cancel = () => {
+    _reject(_status);
     return p;
   };
-  
+  p.isCancel = () => {
+    return isCancel;
+  };
+  return p;
+};
