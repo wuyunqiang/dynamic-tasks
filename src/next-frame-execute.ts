212c79291f555da5fc1d93052da265464a8590b3
wuyunqiang <wuyunqiang@users.noreply.github.com>
2024-04-25 16:14:14 +0800

    Update next-frame-execute.ts

--- src/next-frame-execute.ts
+++ src/next-frame-execute.ts
@@ -1,9 +1,9 @@
 import { Resolve, Task } from "../type/task";
 
 /**
- * 时间切片的方式运行任务
+ * 推迟到下一帧执行任务
  * 避免执行较多任务阻塞UI
- * 使用MessageChannel > requestAnimationFrame > setTimeout兼容
+ * requestAnimationFrame > setTimeout兼容
  * @param {Function} task
  * @returns {Promise}
  */
@@ -15,23 +15,8 @@ export const nextFrameExecute = async (task: Task) => {
     _reject = reject;
   });
 
-  if (typeof MessageChannel !== 'undefined') {
-    const channel = new MessageChannel(); // 建立宏任务的消息通道
-    const port1 = channel.port1;
-    const port2 = channel.port2;
-
-    port2.onmessage = () => {
-      Promise.resolve(task()).then(_resolve, _reject);
-    }
-    port1.postMessage('next')
-    return p;
-  }
-
-  /**下一帧执行执行 
-   * Any rAFs queued in your event handlers will be executed in the same frame.
-   * Any rAFs queued in a rAF will be executed in the next frame
-   * **/
   if (typeof requestAnimationFrame !== "undefined") {
+    // 浏览器兼容 确保推迟到下一帧
     requestAnimationFrame(() => {
       requestAnimationFrame(() => {
         Promise.resolve(task()).then(_resolve, _reject);
